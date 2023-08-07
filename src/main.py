f974e0c35b78010c494ce8306eb37b0ce9c7f099
RyleyGG <griffithryley2000@gmail.com>
2023-08-07 15:29:31 -0400

    uncomment tfidf model

--- src/main.py
+++ src/main.py
@@ -30,8 +30,8 @@ def main():
     print('=== PREFILTERING DATA ===')
     combinedDf = analysis_service.prefilterData(combinedDf)
 
-    # print('=== RUNNING TFIDF MODEL ===')
-    # analysis_service.tfidf(combinedDf)
+    print('=== RUNNING TFIDF MODEL ===')
+    analysis_service.tfidf(combinedDf)
 
     print('=== RUNNING DEEP LEARNING MODEL ===')
     analysis_service.feedForwardNet(combinedDf)
